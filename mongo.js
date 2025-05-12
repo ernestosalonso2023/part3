91be99b8e5e1f5435b4251611a9738852bf7ad59
Ernesto <ernestosalonso2023@gmail.com>
2025-05-12 12:32:53 +0200

    modified:   mongo.js

--- mongo.js
+++ mongo.js
@@ -2,23 +2,17 @@
 const mongoose = require('mongoose')
 let password = null
 let url = null
-
 mongoose.set('strictQuery',false)
 const contactSchema = new mongoose.Schema({
-  //id: number,
   name: String,
   number: String
   
 })
 const Contact = mongoose.model('Contact', contactSchema)
-/*const contact = new Contact({
- // id: 1,
-  name: "Ernesto",
-  number: "040-123456",
-})*/
 
-if (process.argv.length<3) {
+if (process.argv.length<3 || process.argv.length>5) {
   console.log('give password as argument')
+  console.log("Remember If the name contains spaces, it must be enclosed in quotation marks: Example: 'Ernesto Perez'")
   process.exit(1)
 } else if (process.argv.length==3) {
   //  console.log("password",process.argv[2])
@@ -46,13 +40,4 @@ if (process.argv.length<3) {
         console.log('Added',name,'number',number, 'to phonebook')
         mongoose.connection.close()
       })
-   // console.log("name",name)
-    //console.log("number",number)
-}
-//const password = "RDC4oDpQ2WVN73PQ"//process.argv[2]
-//const url = `mongodb+srv://erneyud:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
-
-/*contact.save().then(result => {
-  console.log('person saved!')
-  mongoose.connection.close()
-}) */
+ }
